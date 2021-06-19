35bb852a6ccdb4e69d09282f61ccb10f4d7b17ee
flypig <flypig396@gmail.com>
2021-06-20 01:18:33 +0800

    widget介绍-statelessWidget/statefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,248 +1,176 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
-import 'dart:async' show Future;
-import 'package:flutter/services.dart' show rootBundle;
 
-/// 使用 rootBundle 异步加载
-Future<String> loadAsset() async {
-  return await rootBundle.loadString('assets/logo.png');
-}
+void main() => runApp(MyApp());
 
-/// 加载图片，并且返回 widget
-Widget build(BuildContext context) {
-  return Image.asset('images/feature-1.png');
-}
-
-void main() {
-  runApp(MyApp());
-}
-
-/// App入口
+/// 无状态服务入口
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      title: 'Material App',
+
+      /// 主页调用回显字符
+      // home: Scaffold(
+      //   appBar: AppBar(
+      //     title: Text('Material App Bar'),
+      //   ),
+      //   body: Center(
+      //     child: Container(
+      //       // child: Text('Hello World'),
+      //       child: Echo(text: "hello Widget?"),
+      //     ),
+      //   ),
+      // ),
+
+      /// 主页使用findAncestorStateOfType最近的对象
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('子树中获取State对象'),
+        ),
+        body: Center(
+          child: Builder(builder: (context) {
+            return RaisedButton(
+              onPressed: () {
+                /// 查找父级最近的Scaffold对应的ScaffoldState对象
+                // ScaffoldState _state =
+                //     context.findAncestorStateOfType<ScaffoldState>()!;
+                // //调用ScaffoldState的showSnackBar来弹出SnackBar
+                // _state.showSnackBar(
+                //   SnackBar(
+                //     content: Text("我是SnackBar"),
+                //   ),
+                // );
+
+                /// 直接通过of静态方法来获取ScaffoldState
+                ScaffoldState _state = Scaffold.of(context);
+                _state.showSnackBar(
+                  SnackBar(
+                    content: Text("我是SnackBar1"),
+                  ),
+                );
+              },
+              child: Text("显示SnackBar1"),
+            );
+          }),
+        ),
       ),
 
-      /// 命名注册的路由
-      routes: {
-        "new_page": (context) => NewRoute(),
-        "tip_page": (context) => TipRoute(text: '我是提示xxxx'),
-        "/": (context) => MyHomePage(title: 'Flutter Demo Home Page'), //注册首页路由
-      },
-
-      /// onGenerateRoute回调签名：假设路由表没有注册，
-      /// 就会调用这个函数（商城登陆展示问题）
-      // onGenerateRoute: (RouteSettings settings) {
-      //   return MaterialPageRoute(builder: (context) {
-      //     String? routeName = settings.name;
-      //     // 如果访问的路由页需要登录，但当前未登录，则直接返回登录页路由，
-      //     // 引导用户登录；其它情况则正常打开路由。
-      //     ...
-      //   });
-      // }
-
-      /// 调用根路由选择首页， 不需要再调用MyHomePage
-      // home: MyHomePage(title: 'Flutter Demo Home Page'),
+      /// 主页应用计算器
+      // home: CounterWidget(),
     );
   }
 }
 
-/// 路由页面
-class NewRoute extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("New route"),
-      ),
-      body: Center(
-        child: Text("This is new route"),
-      ),
-    );
-  }
-}
-
-/// 创建传参路由
-class TipRoute extends StatelessWidget {
-  TipRoute({
+/// 实现了一个回显字符串
+class Echo extends StatelessWidget {
+  const Echo({
     Key? key,
-    required this.text, // 接收一个text参数
+    required this.text,
+    this.backgroundColor: Colors.grey,
   }) : super(key: key);
-  final String text;
 
+  final String text;
+  final Color backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: backgroundColor,
+        child: Text(text),
+      ),
+    );
+  }
+}
+
+/// 在子树中获取父级widget
+class ContextRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("提示"),
+        title: Text("Context测试"),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(18),
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Text(text),
-              RaisedButton(
-                onPressed: () => Navigator.pop(context, "我是返回值"),
-                child: Text("返回"),
-              )
-            ],
+      body: Container(
+        child: Builder(builder: (context) {
+          // 在Widget树中向上查找最近的父级`Scaffold` widget
+          Scaffold? scaffold =
+              context.findAncestorWidgetOfExactType<Scaffold>();
+          // 直接返回 AppBar的title， 此处实际上是Text("Context测试")
+          Widget? widget1 = (scaffold!.appBar as AppBar).title;
+          return widget1 ?? Container();
+          // return (scaffold!.appBar as AppBar).title!;
+        }),
+      ),
+    );
+  }
+}
+
+/// 有状态的计数器
+class CounterWidget extends StatefulWidget {
+  const CounterWidget({Key? key, this.initValue: 0});
+
+  final int initValue;
+
+  @override
+  _CounterWidgetState createState() => new _CounterWidgetState();
+}
+
+/// 定义_CounterWidgetState
+class _CounterWidgetState extends State<CounterWidget> {
+  int _counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    //初始化状态
+    _counter = widget.initValue;
+    print("initState");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("build");
+    return Scaffold(
+      body: Center(
+        child: FlatButton(
+          child: Text('$_counter'),
+          //点击后计数器自增
+          onPressed: () => setState(
+            () => ++_counter,
           ),
         ),
       ),
     );
   }
-}
-
-/// 默认主页面
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-/// 定义页面状态
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void didUpdateWidget(CounterWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    print("didUpdateWidget");
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-
-            /// 路由按钮打开新页面
-            FlatButton(
-              child: Text("open new route"),
-              textColor: Colors.blue,
-              onPressed: () {
-                /// 导航到新路由
-                // 1. 使用Navigator.push调用NewRoute
-                // Navigator.push(context, MaterialPageRoute(builder: (context) {
-                //   return NewRoute();
-                // }));
-                /// 2. 使用Navigator.pushNamed调用路由名实现路由
-                Navigator.pushNamed(context, "new_page");
-              },
-            ),
-
-            /// 打开传参页面
-            RaisedButton(
-              onPressed: () async {
-                // 打开`TipRoute`，并等待返回结果
-                var result = await Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return TipRoute(
-                        // 路由参数
-                        text: "我是提示xxxx",
-                      );
-                    },
-                  ),
-                );
-                //输出`TipRoute`路由返回结果
-                print("路由返回值: $result");
-              },
-              child: Text("打开提示页"),
-            ),
-
-            /// 调用生成随机数
-            RandomWordsWidget(),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  void deactivate() {
+    super.deactivate();
+    print("deactive");
   }
-}
 
-/// 使用english_words生成随机字符串
-class RandomWordsWidget extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    // 生成随机字符串
-    final wordPair = new WordPair.random();
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: new Text(wordPair.toString()),
-    );
+  void dispose() {
+    super.dispose();
+    print("dispose");
+  }
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    print("reassemble");
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print("didChangeDependencies");
   }
 }
