6cb3cb26df554a74efcdc3cc68a7cb5044ba517b
flypig <flypig396@gmail.com>
2021-06-20 10:04:03 +0800

    文本样式

--- lib/main.dart
+++ lib/main.dart
@@ -9,371 +9,51 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
 
-      /// 主页调用回显字符
-      // home: Scaffold(
-      //   appBar: AppBar(
-      //     title: Text('Material App Bar'),
-      //   ),
-      //   body: Center(
-      //     child: Container(
-      //       // child: Text('Hello World'),
-      //       child: Echo(text: "hello Widget?"),
-      //     ),
-      //   ),
-      // ),
-
-      /// 主页使用findAncestorStateOfType最近的对象
-      // home: Scaffold(
-      //   appBar: AppBar(
-      //     title: Text('子树中获取State对象'),
-      //   ),
-      //   body: Center(
-      //     child: Builder(builder: (context) {
-      //       return RaisedButton(
-      //         onPressed: () {
-      //           /// 查找父级最近的Scaffold对应的ScaffoldState对象
-      //           // ScaffoldState _state =
-      //           //     context.findAncestorStateOfType<ScaffoldState>()!;
-      //           // //调用ScaffoldState的showSnackBar来弹出SnackBar
-      //           // _state.showSnackBar(
-      //           //   SnackBar(
-      //           //     content: Text("我是SnackBar"),
-      //           //   ),
-      //           // );
-      //           /// 直接通过of静态方法来获取ScaffoldState
-      //           ScaffoldState _state = Scaffold.of(context);
-      //           _state.showSnackBar(
-      //             SnackBar(
-      //               content: Text("我是SnackBar1"),
-      //             ),
-      //           );
-      //         },
-      //         child: Text("显示SnackBar1"),
-      //       );
-      //     }),
-      //   ),
-      // ),
-
-      /// 主页应用计算器
-      // home: CounterWidget(),
-
-      ///  盒子变色
-      //  自身管理
-      // home: TapboxA(),
-
-      // 父管理
-      // home: ParentWidget(),
-
-      // 混合管理
-      home: ParentWidgetC(),
-    );
-  }
-}
-
-/// 实现了一个回显字符串
-class Echo extends StatelessWidget {
-  const Echo({
-    Key? key,
-    required this.text,
-    this.backgroundColor: Colors.grey,
-  }) : super(key: key);
-
-  final String text;
-  final Color backgroundColor;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: backgroundColor,
-        child: Text(text),
-      ),
-    );
-  }
-}
-
-/// 在子树中获取父级widget
-class ContextRoute extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Context测试"),
-      ),
-      body: Container(
-        child: Builder(builder: (context) {
-          // 在Widget树中向上查找最近的父级`Scaffold` widget
-          Scaffold? scaffold =
-              context.findAncestorWidgetOfExactType<Scaffold>();
-          // 直接返回 AppBar的title， 此处实际上是Text("Context测试")
-          Widget? widget1 = (scaffold!.appBar as AppBar).title;
-          return widget1 ?? Container();
-          // return (scaffold!.appBar as AppBar).title!;
-        }),
-      ),
-    );
-  }
-}
-
-/// 有状态的计数器
-class CounterWidget extends StatefulWidget {
-  const CounterWidget({Key? key, this.initValue: 0});
-
-  final int initValue;
-
-  @override
-  _CounterWidgetState createState() => new _CounterWidgetState();
-}
-
-/// 定义_CounterWidgetState
-class _CounterWidgetState extends State<CounterWidget> {
-  int _counter = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    //初始化状态
-    _counter = widget.initValue;
-    print("initState");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print("build");
-    return Scaffold(
-      body: Center(
-        child: FlatButton(
-          child: Text('$_counter'),
-          //点击后计数器自增
-          onPressed: () => setState(
-            () => ++_counter,
+      /// 字符文本
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Text(
+                "Hello world",
+                textAlign: TextAlign.left,
+              ),
+              Text(
+                "Hello world! I'm Jack. " * 4,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Text(
+                "Hello world",
+                textScaleFactor: 1.5,
+              ),
+              Text(
+                "Hello world " * 6, //字符串重复六次
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                "Hello world",
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 18.0,
+                    height: 1.2,
+                    fontFamily: "Courier",
+                    background: new Paint()..color = Colors.yellow,
+                    decoration: TextDecoration.underline,
+                    decorationStyle: TextDecorationStyle.dashed),
+              ),
+              Text.rich(TextSpan(children: [
+                TextSpan(text: "Home: "),
+                TextSpan(
+                  text: "https://flutterchina.club",
+                  style: TextStyle(color: Colors.blue),
+                ),
+              ])),
+            ],
           ),
-        ),
-      ),
-    );
-  }
-
-  @override
-  void didUpdateWidget(CounterWidget oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    print("didUpdateWidget");
-  }
-
-  @override
-  void deactivate() {
-    super.deactivate();
-    print("deactive");
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    print("dispose");
-  }
-
-  @override
-  void reassemble() {
-    super.reassemble();
-    print("reassemble");
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    print("didChangeDependencies");
-  }
-}
-
-/// TapboxA 管理自身状态.
-
-///------------------------- TapboxA ----------------------------------
-
-class TapboxA extends StatefulWidget {
-  TapboxA({Key? key}) : super(key: key);
-
-  @override
-  _TapboxAState createState() => new _TapboxAState();
-}
-
-class _TapboxAState extends State<TapboxA> {
-  bool _active = false;
-
-  void _handleTap() {
-    setState(() {
-      _active = !_active;
-    });
-  }
-
-  Widget build(BuildContext context) {
-    return new GestureDetector(
-      onTap: _handleTap,
-      child: new Container(
-        child: new Center(
-          child: new Text(
-            _active ? 'Active' : 'Inactive',
-            style: new TextStyle(fontSize: 32.0, color: Colors.white),
-          ),
-        ),
-        width: 200.0,
-        height: 200.0,
-        decoration: new BoxDecoration(
-          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
-        ),
-      ),
-    );
-  }
-}
-
-/// ParentWidget 为 TapboxB 管理状态.
-
-//------------------------ ParentWidget --------------------------------
-
-class ParentWidget extends StatefulWidget {
-  @override
-  _ParentWidgetState createState() => new _ParentWidgetState();
-}
-
-class _ParentWidgetState extends State<ParentWidget> {
-  bool _active = false;
-
-  void _handleTapboxChanged(bool newValue) {
-    setState(() {
-      _active = newValue;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Container(
-      child: new TapboxB(
-        active: _active,
-        onChanged: _handleTapboxChanged,
-      ),
-    );
-  }
-}
-
-//------------------------- TapboxB ----------------------------------
-
-class TapboxB extends StatelessWidget {
-  TapboxB({Key? key, this.active: false, required this.onChanged})
-      : super(key: key);
-
-  final bool active;
-  final ValueChanged<bool> onChanged;
-
-  void _handleTap() {
-    onChanged(!active);
-  }
-
-  Widget build(BuildContext context) {
-    return new GestureDetector(
-      onTap: _handleTap,
-      child: new Container(
-        child: new Center(
-          child: new Text(
-            active ? 'Active' : 'Inactive',
-            style: new TextStyle(fontSize: 32.0, color: Colors.white),
-          ),
-        ),
-        width: 200.0,
-        height: 200.0,
-        decoration: new BoxDecoration(
-          color: active ? Colors.lightGreen[700] : Colors.grey[600],
-        ),
-      ),
-    );
-  }
-}
-
-//---------------------------- ParentWidget ----------------------------
-
-class ParentWidgetC extends StatefulWidget {
-  @override
-  _ParentWidgetCState createState() => new _ParentWidgetCState();
-}
-
-class _ParentWidgetCState extends State<ParentWidgetC> {
-  bool _active = false;
-
-  void _handleTapboxChanged(bool newValue) {
-    setState(() {
-      _active = newValue;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Container(
-      child: new TapboxC(
-        active: _active,
-        onChanged: _handleTapboxChanged,
-      ),
-    );
-  }
-}
-
-//----------------------------- TapboxC ------------------------------
-
-class TapboxC extends StatefulWidget {
-  TapboxC({Key? key, this.active: false, required this.onChanged})
-      : super(key: key);
-
-  final bool active;
-  final ValueChanged<bool> onChanged;
-
-  @override
-  _TapboxCState createState() => new _TapboxCState();
-}
-
-class _TapboxCState extends State<TapboxC> {
-  bool _highlight = false;
-
-  void _handleTapDown(TapDownDetails details) {
-    setState(() {
-      _highlight = true;
-    });
-  }
-
-  void _handleTapUp(TapUpDetails details) {
-    setState(() {
-      _highlight = false;
-    });
-  }
-
-  void _handleTapCancel() {
-    setState(() {
-      _highlight = false;
-    });
-  }
-
-  void _handleTap() {
-    widget.onChanged(!widget.active);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // 在按下时添加绿色边框，当抬起时，取消高亮
-    return new GestureDetector(
-      onTapDown: _handleTapDown, // 处理按下事件
-      onTapUp: _handleTapUp, // 处理抬起事件
-      onTap: _handleTap,
-      onTapCancel: _handleTapCancel,
-      child: new Container(
-        child: new Center(
-          child: new Text(widget.active ? 'Active' : 'Inactive',
-              style: new TextStyle(fontSize: 32.0, color: Colors.white)),
-        ),
-        width: 200.0,
-        height: 200.0,
-        decoration: new BoxDecoration(
-          color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
-          border: _highlight
-              ? new Border.all(
-                  color: Colors.teal[700]!,
-                  width: 10.0,
-                )
-              : null,
         ),
       ),
     );
